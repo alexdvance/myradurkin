0e9a9996c9326bbe6e83b1f765a1f06d8ae79c2e
Alex Vance <alexdvance@gmail.com>
2016-03-14 00:21:07 -0400

    Added basic sass compilation to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-// Set up gulp
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./css/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('default', function () {
+  gulp.watch('./css/sass/*.scss', ['sass']);
+});
