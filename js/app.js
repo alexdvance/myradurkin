e76e1fd06f6d640bdfd85fb9deaf73639bc88cf7
Alex Vance <alexdvance@gmail.com>
2016-03-18 17:11:01 -0400

    Added routing for website

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
 angular
-  .module('MyraDurkin', [])
+  .module('MyraDurkin', [
+    'ngRoute'
+  ])
+  .config([
+    '$routeProvider',
+    '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider.
+        when('/', {
+          templateUrl: 'templates/_home.html',
+          controller: 'MainController'
+        }).
+        when('/about', {
+          templateUrl: 'templates/_about.html',
+          controller: 'MainController'
+        }).
+        when('/practice', {
+          templateUrl: 'templates/_practice.html',
+          controller: 'MainController'
+        }).
+        when('/services', {
+          templateUrl: 'templates/_services.html',
+          controller: 'MainController'
+        }).
+        otherwise({
+          redirectTo: '/#/'
+        });
+
+      // $locationProvider.html5Mode(true);
+  }])
   .controller('MainController', ['$scope', function($scope) {
     // code
     console.log('hey dog');
