62e1403366cbfc30d0739dd3ebc928a20186c1e6
Alex Vance <alexdvance@gmail.com>
2016-04-24 15:23:11 -0400

    Underline name of current page in nav bar

--- js/app.js
+++ js/app.js
@@ -9,19 +9,27 @@ angular
       $routeProvider.
         when('/', {
           templateUrl: 'templates/_home.html',
-          controller: 'MainController'
+          controller: 'MainController',
+          title: 'Myra Durkin, LICSW',
+          page: 'home'
         }).
         when('/about', {
           templateUrl: 'templates/_about.html',
-          controller: 'MainController'
+          controller: 'MainController',
+          title: 'Myra Durkin, LICSW | About',
+          page: 'about'
         }).
         when('/practice', {
           templateUrl: 'templates/_practice.html',
-          controller: 'MainController'
+          controller: 'MainController',
+          title: 'Myra Durkin, LICSW | Practice',
+          page: 'practice'
         }).
         when('/services', {
           templateUrl: 'templates/_services.html',
-          controller: 'MainController'
+          controller: 'MainController',
+          title: 'Myra Durkin, LICSW | Services',
+          page: 'services'
         }).
         otherwise({
           redirectTo: '/#/'
@@ -29,6 +37,13 @@ angular
 
       // $locationProvider.html5Mode(true);
   }])
+  .run(function($rootScope, $route) {
+    $rootScope.$on("$routeChangeSuccess", function(currentRoute, previousRoute){
+      //Change page title, based on Route information
+      $rootScope.title = $route.current.title;
+      $rootScope.page = $route.current.page;
+    });
+  })
   .controller('MainController', ['$scope', function($scope) {
     // code
     console.log('hey dog');
